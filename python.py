6d5797fa6eba7f0fab375b81212543b4d87a588d
HuerRome <4n.rooo@gmail.com>
2022-09-18 02:21:22 -0300

    change the background

--- python.py
+++ python.py
@@ -1,44 +1,49 @@
-def principal():
-    print("Peso piedras de Lucia: ")
-    LuciaP1 = float(input("Ingrese peso piedra 1: "))
-    LuciaP2 = float(input("Ingrese peso piedra 2: "))
-    print("Peso piedras de Juana: ")
-    JuanaP1 = float(input("Ingrese peso piedra 1: "))
-    JuanaP2 = float(input("Ingrese peso piedra 2: "))
-    print("Peso piedras de Javier: ")
-    JavierP1 = float(input("Ingrese peso piedra 1: "))
-    JavierP2 = float(input("Ingrese peso piedra 2: "))
-    print("Peso piedras de Andres: ")
-    AndresP1 = float(input("Ingrese peso piedra 1: "))
-    AndresP2 = float(input("Ingrese peso piedra 2: "))
 
-    LuciaPT = LuciaP1 + LuciaP2
-    JuanaPT = JuanaP1 + JuanaP2
-    JavierPT = JavierP1 + JavierP2
-    AndresPT = AndresP1 + AndresP2
+def menu():
+    print("------------Menu de restaurante---------------")
+    print("Si su compra supera los 600$ se hara un descuento del 10%")
+    print("1- Pizza de muzarella------------ 550$")
+    print("2- Pizza especial---------------- 750$")
+    print("3- Milanesa napolitana------------ 850$")
 
-    if LuciaPT>JuanaPT and LuciaPT>JavierPT and LuciaPT>AndresPT:
-        print("Lucia tiene las piedras de mayor peso: ", LuciaPT)
-    elif JuanaPT>LuciaPT and JuanaPT>JavierPT and JuanaPT>AndresPT:
-        print("Lucia tiene las piedras de mayor peso: ", JuanaPT)
-    elif JavierPT>JuanaPT and JavierPT>LuciaPT and JavierPT>AndresPT:
-        print("Lucia tiene las piedras de mayor peso: ", JavierPT)
-    elif AndresPT>JuanaPT and AndresPT>JavierPT and AndresPT>LuciaPT:
-        print("Lucia tiene las piedras de mayor peso: ", AndresPT)
+
+    opcion = int(input("Por favor ingrese una opcion: "))
+    if opcion < 1 or opcion > 3:
+        print("Ingreso una opcion incorrecta. Intente de nuevo\n")
+        menu()
     else:
-        print("sos re pavo")
-    promedioPT = (LuciaPT + AndresPT + JavierPT + JuanaPT)/4
-    print("El peso promedio de los petes igual a: ", promedioPT)
-#principal()
+        return opcion
+
+
+def opciones():
+    opcion = menu()
+    if opcion == 1:
+        total = 550
+        print('Su pedido es menor a 600$, no se aplica descuento')
+        print(f"Su total a pagar es {total}$")
+        return total
+    elif opcion == 2:
+        descuento = (750/100) * 10
+        resta = 750 - descuento
+        print(f"Su compra supero los 600$ se aplica descuento. Total a pagar {resta}$")
+        return resta
+    else:
+        descuento = (800/100) * 10
+        resta = 800 - descuento
+        print(f"Su compra supero los 600$ se aplica descuento. Total a pagar {resta}$")
+        return resta
+opciones()
+
+
+def opciones_pago():
+    print("Por favor ingrese una opcion de pago")
+    print("a- Devito ")
+    print("b- Efectivo")
+    print("c-Transferencia")
+    opcion = input("Ingres")
 
 
 
 
 
 
-
-
-
-
-    
-    
